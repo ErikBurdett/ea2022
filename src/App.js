2320958f19765fa2b75f617d52e473ac225d13b7
ErikBurdett <erikaburdet@gmail.com>
2022-05-28 14:37:21 -0500

    adds mdb forms, nodemailer, express, cors  - mdb updated needed next

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import Services from './components/pages/Services';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Navbar from './components/nav/Navbar';
+import ContactForm from './components/contactform/ContactForm';
+import ContactForm2 from './components/contactform/ContactForm2';
 
 
 function App() {
@@ -16,10 +20,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' exact element={<Home/>}/>
-        <Route path='/services' element={Services}/>
-        <Route path='/about' element={About}/>
-        <Route path='/contact' element={Contact}/>
-        <Route path='/projects' element={Projects}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<ContactForm/>}/>
+        <Route path='/projects' element={<Projects/>}/>
       </Routes>
     </Router>
     </>
